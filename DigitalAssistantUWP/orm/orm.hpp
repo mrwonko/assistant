11d2775607d6cc6ed6b4d330f4f6c941b49d7110
Willi Schinmeyer <willi@schinmeyer.de>
2016-11-14 21:10:00 +0100

    moved str_const to own file

--- DigitalAssistantUWP/orm/orm.hpp
+++ DigitalAssistantUWP/orm/orm.hpp
@@ -5,6 +5,8 @@
 #include <algorithm>
 #include <tuple>
 
+#include "str_const.hpp"
+
 /*
 # Concept
 
@@ -30,34 +32,6 @@ A table is defined by:
 * fields
 * maybe constraints later
 */
-
-/// @author Scott Schurr, Willi Schinmeyer
-class str_const { // constexpr string
-public:
-	template< std::size_t N >
-	constexpr str_const( const char( &a )[ N ] )
-		: _data( a )
-		, _size( N - 1 ) {
-	}
-	constexpr const char operator[]( std::size_t n ) const {
-		return n < _size ? _data[ n ] :
-			throw std::out_of_range( "" );
-	}
-	constexpr const std::size_t size() const {
-		return _size;
-	}
-	constexpr const bool operator==( const str_const& rhs ) const {
-		return size() == rhs.size() && ( size() == 0 || ( _data[ 0 ] == rhs[ 0 ] && str_const{ _data + 1, _size - 1 } == str_const{ rhs._data + 1, rhs._size - 1 } ) );
-	}
-private:
-	constexpr str_const( const char* const data, const std::size_t size )
-		: _data( data )
-		, _size( size ) {
-	}
-
-	const char* const _data;
-	const std::size_t _size;
-};
 
 // Type-Safe SQL Statements
 namespace sql {
