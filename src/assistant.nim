6fd91604adbaa2dff14643423143a7d2b46b84a5
Willi Schinmeyer <willi.schinmeyer@freiheit.com>
2018-03-22 22:07:35 +0100

    on backend compilation compile frontend and store result in constant
    Words can hardly describe how awesome this is.

--- src/assistant.nim
+++ src/assistant.nim
@@ -1,3 +1,22 @@
 import assistantpkg/shared/hello
+import macros
+import strformat
+import strutils
 
-echo helloString
+proc compileFrontend(): string {.compileTime.} =
+    hint "[building frontend...]"
+    let (log, exitCode) = gorgeEx("nim js assistantpkg/frontend/main")
+    if exitCode != 0:
+        error &"error code {exitCode} compiling frontend:\n{log}"
+    if log != "":
+        let indentedLog = indent(log, 4)
+        hint &"[frontend build succesful. logs:]\n{indentedLog}"
+    hint "[reading compiled frontend...]"
+    const code = staticRead("assistantpkg/frontend/nimcache/main.js")
+    hint "[frontend successfully compiled and read]"
+    return code
+
+const frontendCode = compileFrontend()
+
+echo &"// code that will output {helloString}:"
+echo frontendCode
